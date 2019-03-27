53f5760b8db2bf79011ac355270715483c0c9f5b
Austin Jackson <themacphage@gmail.com>
2019-03-27 18:58:28 +0000

    this works

--- script.py
+++ script.py
@@ -1,46 +1,37 @@
 #!/usr/bin/env python3
 
-from bs4 import BeautifulSoup
-import os
 import csv
 import argparse
+import requests
+import json
 
 # cli arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("courses_txt", help="The path to the courses.txt file. (Default: courses.txt)")
 args = parser.parse_args()
 
-classes = []
+courses = []
 
 with open(args.courses_txt) as f:
-	line = f.readline()
-	line = line.rstrip()
-	classes.append(line)
-	course = line.split(" ")[0]
-	code = line.split(" ")[1]
-	cmd = f'curl \"https://anex.us/grades/?dept={course}&number={code}\" -o table-{course}-{code}.html'
-	print(cmd)
-	os.system(cmd)
+	for line in f:
+		line = line.rstrip()
+		courses.append(line)
+		dept = line.split(" ")[0]
+		number = line.split(" ")[1]
+		r = requests.post("https://anex.us/grades/getData/", data={"dept": dept, "number": number})
+		data = json.loads(r.text)
 
+		output_rows = []
+		output_rows.append(list(data["classes"][0].keys()))
+		#print(output_rows[0])
+		for row in data["classes"]: # iterate over objs
+			new_row = []
+			for key in list(data["classes"][0].keys()): # iterate over keys
+				new_row.append(row[key])
+			#print(new_row)
+			output_rows.append(new_row)
 
-
-for str in classes:
-	course = str.split(" ")[0]
-	code = str.split(" ")[1]
-
-	html = open(f'table-{course}-{code}.html').read()
-	soup = BeautifulSoup(html, features="html.parser")
-	table = soup.find("table")
-	print(table)
-
-	output_rows = []
-	for table_row in table.findAll("tr"):
-		columns = table_row.findAll("td")
-		output_row = []
-		for column in columns:
-			output_row.append(column.text)
-			output_rows.append(output_row)
-
-	with open(f'grades-{course}-{code}.csv', "wb") as csvfile:
-		writer = csv.writer(csvfile)
-		writer.writerows(output_rows)
+		#print(output_rows)
+		with open(f'grades-{dept}-{number}.csv', "w") as csvfile:
+			writer = csv.writer(csvfile)
+			writer.writerows(output_rows)
